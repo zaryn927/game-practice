e04b8a4a4a386a4cdec4d38e820e41c09e893902
Sky Link <s.z_link@yahoo.com>
2017-07-03 08:56:33 -0600

    UI. Trying to load image.

---
+++ src/edu/cnm/deepdive/game/SpriteSheet.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.game;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * @author Sky Link
+ *
+ */
+public class SpriteSheet  {
+
+  private BufferedImage image;
+  
+  public SpriteSheet(BufferedImage image) {
+    this.image = image;
+  }
+  
+  public BufferedImage grabImage(int col, int row, int width, int height) {
+    BufferedImage img = image.getSubimage((col * 32) - 32, (row * 32) - 32, width, height);
+    return img;
+  }
+}
